98a68a0ad1349e9867dd61cc58e0224f758137bb
AzureVauban <haloringman@outlook.com>
2022-12-03 01:54:00 +0000

    Dec 2, 2022, 7:54 PM

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
         std::cout << Playlist.getData() << std::endl;
         Playlist.dequeue();
     }
-    //test queue empty()
+    // test queue empty()
     Queue::Queue New_Playlist;
     for (int i = 0; i < 5; i++)
     {
