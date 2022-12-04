eff0e4c2bd424d12da5f3c4372af75e782839768
AzureVauban <haloringman@outlook.com>
2022-12-04 21:58:14 +0000

    Dec 4, 2022, 3:58 PM

--- containers.h
+++ containers.h
@@ -16,8 +16,22 @@ namespace List
             Node *Previous;
 //setters            
             //void setIndex(int Index) { this->Index = Index; }
-            void setNext(Node *Next) { this->Next = Next; }
-            void setPrevious(Node *Previous) { this->Previous = Previous; }
+        public:
+            void setNext(Node *Next) { 
+                this->Next = Next; 
+                //if the next does not have previous pointer, set it to this
+                if (Next){ //TODO TEST THIS OUT IN DEBUG TO SEE IF IT SHOULD BE IF(!NEXT)
+                    Next->setPrevious(this);
+                }
+                }
+        private:    
+            void setPrevious(Node *Previous) { 
+                this->Previous = Previous; 
+                //if the previous does not have next pointer, set it to this
+                if (Previous){ //TODO TEST THIS OUT IN DEBUG TO SEE IF IT SHOULD BE IF(!PREVIOUS)
+                    Previous->setNext(this);
+                }
+            }
 //getters
             const int &getIndex() const { return this->Index; }
         public:
@@ -25,6 +39,7 @@ namespace List
             private:
             Node *getPrevious() const { return this->Previous; }
             //const std::string &getData() const { return this->Data; }
+        public:
 //constructor
             explicit Node(Node* Previous, Node* Next){
                 this->setPrevious(Previous);
@@ -52,18 +67,23 @@ namespace List
             }
             this->size = new_size;
         }
+        //std::cout << "size of " << this << " has be set to " << this->getSize() << std::endl;
     }
 //getters
 
 //returns the head Node if the linked list class
     Node* getHead() const { return this->head; }
+public:
+//returns the amount of nodes in the linked list instance
+    const int& getSize() const { return this->size; }
+    
 //functions
 private:
 // returns the Node at the end of the list
      Node* getEnd() {
         Node * current = this->getHead();
         //traverse to the end of the current linked list
-        while (current->getNext()) {
+        while (current->getNext() && current) {
             current = current->getNext();
         }
         return current;
@@ -72,11 +92,18 @@ public:
 // returns true if the head node is null
     const bool isEmpty() { return this->head == nullptr; }
 // appends an item do the end of the linked List
-    
+    void append(/*add data parameter here*/){ //TODO DEBUG - WHEN endpoint is NULL THIS CAUSES A SEG-FAULT
+        //traverse to the end of the list
+        Node* endpoint = this->getEnd();
+        //append a new Node
+        // TODO ADD THE /*DATA*/ PARAMETER IN BETWEEN endpoint,nullptr
+        endpoint->setNext(new Node(endpoint,nullptr));
+    }
 public:
 //constructor
         explicit List(){
             this->setHead(nullptr);
+            this->setSize();
         }
     };
 // end of List definition
--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include "containers.h"
 
-int main()
-{
-    List::List myList;
-    if (myList.isEmpty())
+void evaluate_linked_list(List::List &linked_list){
+    if (linked_list.isEmpty())
     {
         std::cout << "my list is empty" << std::endl;
+        //append something to the list
+        linked_list.append();
+        evaluate_linked_list(linked_list);
     }
     else
     {
         // output the size of the list
-        int size = 0;
+        int size = linked_list.getSize();
         std::cout << "my list has " << size << " elements!" << std::endl;
     }
+}
+int main()
+{
+    List::List myList;
+    evaluate_linked_list(myList)   ;
+    int end_of_process_size = myList.getSize();
     std::cout << "terminating process..." << std::endl;
     return 0;
 }
