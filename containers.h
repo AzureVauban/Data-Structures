465f2f86f6b8037adc2d2b879d992a60df77b66f
AzureVauban <haloringman@outlook.com>
2022-12-04 22:21:15 +0000

    Dec 4, 2022, 4:21 PM

--- containers.h
+++ containers.h
@@ -9,7 +9,7 @@ namespace List
 {
     class Node
     {
-   
+
         /* functionality:
         - automatically link node instances to one another when instantiated
         - automatically set the index of all connected nodes instances when instantiated
@@ -18,17 +18,55 @@ namespace List
         Node *Previous;
         int Index;
         std::string Data;
+
     public:
         // setters
-        void setNext(Node *next) { Next = next; }
-        void setPrevious(Node *previous) { Previous = previous; }
-        void setIndex(int index) { Index = index; }
+        void setNext(Node *next)
+        {
+            Next = next;
+            // set the index of the next node
+            Next->setIndex(Index + 1);
+            // if the next pointer is not null, set the previous pointer of the next instance to this
+            if (Next != nullptr)
+            {
+                Next->setPrevious(this);
+            }
+        }
+        void setPrevious(Node *previous)
+        {
+            Previous = previous;
+            // set the index of the previous node
+            Previous->setIndex(Index - 1);
+            // if the previous pointer is not null, set the next pointer of the previous instance to this
+            if (Previous != nullptr)
+            {
+                Previous->setNext(this);
+            }
+        }
+        void setIndex(const int index) { Index = index; }
         void setData(std::string data) { Data = data; }
         // getters
         Node *getNext() const { return Next; }
         Node *getPrevious() const { return Previous; }
         const int getIndex() const { return Index; }
         std::string getData() const { return Data; }
+        // constructor
+        explicit Node(Node *previous, std::string Data, Node *next)
+        {
+            setIndex(0);
+            setPrevious(previous);
+            setData(std::move(Data));
+            setNext(next);
+            // set the index of the current node
+        }
+        // destructor
+        ~Node()
+        {
+            // delete the next & previous node
+            delete Next;
+            delete Previous;
+            std::cout << "deleting node" << std::endl;
+        }
     }; // TODO TEST OUT NODE CLASS FIRST
 }
 // end of containers.h
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "containers.h"
 
-int main(){
-    //test out the Node class first
+int main()
+{
+    // test out the Node class first
+    using namespace List;
+    Node head(nullptr, "head", nullptr);
+
     std::cout << "terminating process" << std::endl;
     return 0;
 }
