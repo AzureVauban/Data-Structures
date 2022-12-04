c1d1cad18261539b4b07c0cffde3d25638a49c24
AzureVauban <haloringman@outlook.com>
2022-12-04 20:50:29 +0000

    Dec 4, 2022, 2:50 PM

--- main.cpp
+++ main.cpp
@@ -1,61 +1,11 @@
 #include "containers.h"
-/*
-void queue_demo()
-{
-    // queue demo
-    Queue::Queue Playlist;
-    Playlist.enqueue("Carpenter Brut - Roller Mobster");
-    Playlist.enqueue("Deadlife - Deviant");
-    Playlist.enqueue("Perturbator - Raining Steel");
-    Playlist.enqueue("Carpenter Brut - End Titles");
-    Playlist.enqueue("Deadlife - Downpour");
-    Playlist.enqueue("Perturbator - Sexualizer");
-    std::cout << "Songs in my Synthwave Music Queue" << std::endl;
-    for (int i = Playlist.getSize(); i > 0; i--)
-    {
-        std::cout << Playlist.getData() << std::endl;
-        Playlist.dequeue();
-    }
-    // empty() test
-    Queue::Queue New_Playlist;
-    for (int i = 0; i < 5; i++)
-    {
-        New_Playlist.enqueue("UNKNOWN ARTIST - TOP SECRET SONG");
-        Playlist.enqueue("UNKNOWN ARTIST - TOP SECRET SONG");
-    }
-    New_Playlist.empty();
-    //! add a debug point here and see the queues in debug =)
-    Playlist.~Queue();
-}
-*/
+
 
 int main()
-{
-    // queue_demo();
 
-    List::List IDEs;
-    IDEs.append("Visual Studio Code");
-    IDEs.append("Visual Studio");
-    IDEs.append("CLion");
-    IDEs.append("CodeBlocks");
-    IDEs.append("Replit");
-    IDEs.append("Pycharm");
-    IDEs.append("Vim");
-    // print list
-    const int listsize = IDEs.getSize();
-    for (int i = -1; i <= listsize; i++)
-    {
-        std::cout << IDEs.getData(i) << std::endl;
-    }
-    for (int i = 0; i != listsize * 2; i++)
-    {
-        IDEs.pop();
-    }
-    // print list
-    for (int i = -1; i <= listsize; i++)
-    {
-        std::cout << IDEs.getData(i) << std::endl;
-    }
+
+    List::List test;
+    test.head->test_output_node();
     std::cout << "terminating process..." << std::endl;
     return 0;
 }
