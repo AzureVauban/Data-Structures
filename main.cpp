97fc4eaf47026867170a1ae8b60736ad4782e6c8
AzureVauban <haloringman@outlook.com>
2022-12-05 03:03:10 +0000

    Dec 4, 2022, 9:03 PM

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     // test out the Node class first
     Node node_one(nullptr, "one", nullptr);
-    Node node_two(node_one, "two", nullptr);
+    Node node_two(&node_one, "two", nullptr);
 
     std::cout << "terminating process" << std::endl;
     return 0;
