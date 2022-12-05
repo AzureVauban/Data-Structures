aa7538cc59f2f9d4c1e19defa5bcefbdb94b0852
AzureVauban <haloringman@outlook.com>
2022-12-05 04:21:41 +0000

    Dec 4, 2022, 10:21 PM

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main()
     // Node(/*PREVIOUS*/,/*DATA*/,/*DATA*/)
     //Node purple(nullptr, "one", nullptr);
     Queue waitlist;
+    waitlist.enqueue("Bob");
+    waitlist.enqueue("Michael");
+    waitlist.enqueue("Jenny");
     std::cout << "terminating process" << std::endl;
     return 0;
 }
