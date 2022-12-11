1a2a41ea07d7bd56ca2927de9ea0e50c768189f0
AzureVauban <haloringman@outlook.com>
2022-12-11 06:22:36 +0000

    Dec 11, 2022, 12:22 AM

---
+++ List.h
@@ -0,0 +1,13 @@
+#ifndef LINK_LIST_H_
+#define LINK_LIST_H_
+
+#include "Nodebase.h"
+
+namespace List
+{
+    template <class T>
+    class List
+    {
+    };
+}
+#endif // !LINK_LIST_H_
--- main.cpp
+++ main.cpp
@@ -45,8 +45,8 @@ void comparison_demo(Queue::Queue<long long int> &Queue,
 {
     for (int i = 1; i <= 10; i++)
     {
-        Queue.enqueue(fibsequence(i+1));
-        Stack.push(fibsequence(i+1));
+        Queue.enqueue(fibsequence(i + 1));
+        Stack.push(fibsequence(i + 1));
     }
     std::cout << "Peaking Queue: " << Queue.peak() << std::endl
               << "Peaking Stack: " << Stack.peak() << std::endl;
@@ -55,6 +55,8 @@ void comparison_demo(Queue::Queue<long long int> &Queue,
         std::cout << "Dequeuing (From Queue): " << Queue.dequeue() << " | "
                   << "Popping (From Stack): " << Stack.pop() << std::endl;
     }
+    std::cout << "Peaking Queue: " << Queue.peak() << std::endl
+              << "Peaking Stack: " << Stack.peak() << std::endl;
 }
 int main()
 {
