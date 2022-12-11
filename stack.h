7ecd0df971b6ac3af17c6ad534c7460d7e0d02f0
AzureVauban <haloringman@outlook.com>
2022-12-11 05:34:36 +0000

    Dec 10, 2022, 11:34 PM

--- stack.h
+++ stack.h
@@ -37,35 +37,20 @@ namespace Stack
 
         void push(T Data)
         {
-            this->Size += 1;
             if (this->isEmpty())
             {
+                this->Size += 1;
                 // overwrite the head pointer
                 this->Head = new Node<T>(nullptr, Data, nullptr);
             }
-            else
-            {
-                // Node<T> *new_node = new Node(PREV PTR, NEW DATA, NEXT PTR);
-                // get the endpoint
-                Node<T> *endpoint = this->getEnd();
-                endpoint->setNext(new Node<T>(endpoint, Data, nullptr));
-                // this->getEnd()->setIndex(this->getEnd()->getIndex() + 1);
-            }
+
             // set the next index
             this->getEnd()->setIndex(this->getSize() - 1);
         }
 
         T peak() { return this->getHead()->getData(); }
 
-        T pop()
-        {
-            Node<T> *endpoint = this->getEnd();
-            T return_value = endpoint->getData();
-            endpoint->getPrev()->setNext(nullptr);
-            delete endpoint;
-            this->Size -= 1;
-            return return_value;
-        }
+        T pop() { /* ADD NEW CODE HERE*/ }
 
     public:
         explicit Stack()
@@ -75,10 +60,6 @@ namespace Stack
         }
         ~Stack()
         {
-            while (this->Size != 1)
-            {
-                this->pop();
-            }
             // delete Head;
             this->Size = 0;
             std::cout << "Destroyed Queue object at " << this << std::endl;
